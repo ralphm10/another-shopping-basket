273336d8cd1a99393b78a90868059cd7d80b5dcf
Ralph Mallett <rmallet6@ford.com>
2024-09-06 10:31:38 +0100

    refactor - extract function

--- lib/src/main/kotlin/org/example/ShoppingBasket.kt
+++ lib/src/main/kotlin/org/example/ShoppingBasket.kt
@@ -4,12 +4,14 @@ class ShoppingBasket(var items: MutableList<GroceryItem> = mutableListOf()) {
     fun isEmpty(): Boolean = items.isEmpty()
 
     fun add(item: GroceryItem) {
-        require(!items.contains(item)) {"${item.description} already in basket"}
+        require(!containsItem(item)) {"${item.description} already in basket"}
         items.add(item)
     }
 
     fun remove(item: GroceryItem) {
-        require(items.contains(item)) {"${item.description} not in basket"}
+        require(containsItem(item)) {"${item.description} not in basket"}
         items.remove(item)
     }
+
+    private fun containsItem(item: GroceryItem) = items.contains(item)
 }
